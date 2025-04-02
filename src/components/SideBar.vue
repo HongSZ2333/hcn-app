a33bb78cb66e8da9002477f00fc8a11d48ef4df8
Hong_SZ <136214287+HongSZ2333@users.noreply.github.com>
2025-04-03 00:17:32 +0800

    delete: 删除了一些不必要内容

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="box">
     <t-menu theme="light" default-value="dashboard" style="margin-right: 40px">
+      <template #logo>
+        <h2>HCN 控制台</h2>
+      </template>
       <t-menu-item value="dashboard">
         <template #icon>
           <t-icon name="dashboard" />
@@ -13,48 +16,12 @@
         </template>
         资源列表
       </t-menu-item>
-      <t-menu-item value="root">
-        <template #icon>
-          <t-icon name="root-list" />
-        </template>
-        根目录
-      </t-menu-item>
-      <t-menu-item value="control-platform">
-        <template #icon>
-          <t-icon name="control-platform" />
-        </template>
-        调度平台
-      </t-menu-item>
-      <t-menu-item value="precise-monitor">
-        <template #icon>
-          <t-icon name="precise-monitor" />
-        </template>
-        精准监控
-      </t-menu-item>
-      <t-menu-item value="mail">
-        <template #icon>
-          <t-icon name="mail" />
-        </template>
-        消息区
-      </t-menu-item>
-      <t-menu-item value="user-circle">
-        <template #icon>
-          <t-icon name="user-circle" />
-        </template>
-        个人中心
-      </t-menu-item>
-      <t-menu-item value="play-circle">
-        <template #icon>
-          <t-icon name="play-circle" />
-        </template>
-        视频区
-      </t-menu-item>
-      <t-menu-item value="edit1">
-        <template #icon>
-          <t-icon name="edit-1" />
-        </template>
-        资源编辑
-      </t-menu-item>
+<!--      <t-menu-item value="user-circle">-->
+<!--        <template #icon>-->
+<!--          <t-icon name="user-circle" />-->
+<!--        </template>-->
+<!--        个人中心-->
+<!--      </t-menu-item>-->
     </t-menu>
   </div>
 </template>
